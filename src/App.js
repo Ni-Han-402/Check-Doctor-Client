5b316fc21707cae8f6649510a05ee22acd07dc62
Nihan Zyan <nihanzyan38@gmail.com>
2022-05-12 13:03:11 +0600

    Navber With Route Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navber from './Shared/Navber';
+import { Routes, Route} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Appoinment from './Pages/Home/Appoinment';
+import Reviews from './Pages/Home/Reviews';
+import ContactUs from './Pages/Home/ContactUs';
+import About from './Pages/Home/About';
+import Login from './Pages/Home/Login';
+import NotFound from './Pages/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="appoinment" element={<Appoinment />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="contact" element={<ContactUs />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
